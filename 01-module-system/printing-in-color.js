a32be6fed4c9cced4141a54f1245b9118a530dd6
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-07-19 01:04:11 -0300

    add another example of node module

---
+++ 01-module-system/printing-in-color.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+
+console.log(chalk.yellow.bgMagenta.bold('Hello World!'));
+console.log(chalk.blue.underline('Our notes...'));
+console.log(chalk.green('Success!'));
