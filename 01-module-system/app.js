e2d1a8b59da10b239086eae3be1c6e20c42ae01a
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-07-19 00:50:02 -0300

    using npm modules

--- 01-module-system/app.js
+++ 01-module-system/app.js
@@ -2,6 +2,8 @@
 const fs = require('fs');
 const add = require('./utils.js');
 const getNotes = require('./notes.js');
+// Importing NPM modules
+const validator = require('validator');
 
 // Create and override the file
 fs.writeFileSync('notes.txt', 'This file was modified by Node.js!');
@@ -12,3 +14,9 @@ fs.appendFileSync('notes.txt', '\nLet\'s append that.');
 // console.log(name);
 console.log(add(3, 5));
 console.log(getNotes());
+
+// using 'validator' module
+console.log(validator.isEmail('developer.brunv@gmail.com'));
+console.log(validator.isEmail('developer.brunvgmail.com'));
+
+console.log(validator.isURL('http://brunv.com'));
