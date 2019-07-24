db98a03a68bbd21b860549bf93e3b0a5f692caa0
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-07-23 22:01:49 -0300

    i/o and json

--- playground/01-json.js
+++ playground/01-json.js
@@ -1,12 +1,34 @@
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday'
-};
+const fs = require('fs');
 
-// object to JSON:
-const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// };
 
-// JSON to object:
-const parsedBook = JSON.parse(bookJSON);
-console.log(parsedBook.author);
+// // object to JSON:
+// const bookJSON = JSON.stringify(book);
+// console.log(bookJSON);
+
+// // JSON to object:
+// const parsedBook = JSON.parse(bookJSON);
+// console.log(parsedBook.author);
+
+// // Write out the JSON:
+// fs.writeFileSync('01-json.json', bookJSON);
+
+// // Read the JSON file:
+// const dataBuffer = fs.readFileSync('01-json.json');
+// // const dataJSON = dataBuffer.toString();
+// // const data = JSON.parse(dataJSON);
+// const data = JSON.parse(dataBuffer);
+// console.log(data.title);
+
+/* Exercise: */
+const dataBuffer = fs.readFileSync('01-exercise.json');
+const user = JSON.parse(dataBuffer);
+
+user.name = 'Bruno';
+user.age = '23';
+
+const userJSON = JSON.stringify(user);
+fs.writeFileSync('01-exercise.json', userJSON);
