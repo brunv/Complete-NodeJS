53cb13d87b6eb9e3fccd627803a8752dc0d271f1
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-07-29 12:19:36 -0300

    add a forecast

--- 03-asynchronous-nodejs/app.js
+++ 03-asynchronous-nodejs/app.js
@@ -1,8 +1,15 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/121b485e513f0931a1e805a7633c4100/37.8267,-122.4233';
+const url = 'https://api.darksky.net/forecast/121b485e513f0931a1e805a7633c4100/37.8267,-122.4233?units=si';
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.currently);
+request({ url: url, json: true }, (error, response) => {
+    const data = response.body;
+    currentForecast(data);
 });
+
+function currentForecast(data) {
+    const { temperature, precipProbability } = data.currently;
+    const { summary } = data.daily.data[0];
+    const forecast = `${summary}. It is currently ${temperature}ºC degrees out. There is a ${precipProbability}% chance of rain.`;
+    console.log(forecast);
+}
