84ab0dd58d81f0bbfe9b514de4ec0011161d5680
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-07-30 18:06:50 -0300

    refactor with destructuring

--- 03-asynchronous-nodejs/app.js
+++ 03-asynchronous-nodejs/app.js
@@ -6,20 +6,20 @@ const location = process.argv[2];
 if (!location) {
     console.log('Please provide a location.');
 } else {
-    console.log('Getting forecast for ' + location + '...');
+    console.log(`Getting forecast for ${location}...`);
 
-    geocode(location, (error, data) => {
+    geocode(location, (error, { latitude, longitude, location }) => {
         if (error) {
             return console.log(error);
         }
 
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, { summary, temperature, precipProbability }) => {
             if (error) {
                 return console.log(error);
             }
 
-            console.log(data.location);
-            console.log(`${forecastData.summary} It is currently ${forecastData.temperature}°C out. There is a ${forecastData.precipProbability}% chance of rain.`);
+            console.log(location);
+            console.log(`${summary} It is currently ${temperature}°C out. There is a ${precipProbability}% chance of rain.`);
         });
     });
 }
--- 03-asynchronous-nodejs/utils/forecast.js
+++ 03-asynchronous-nodejs/utils/forecast.js
@@ -3,16 +3,16 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/121b485e513f0931a1e805a7633c4100/${latitude},${longitude}?units=si`;
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to the weather service!', undefined);
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
             callback(undefined, {
-                temperature: response.body.currently.temperature,
-                precipProbability: response.body.currently.precipProbability,
-                summary: response.body.daily.data[0].summary
+                temperature: body.currently.temperature,
+                precipProbability: body.currently.precipProbability,
+                summary: body.daily.data[0].summary
             });
         }
     });
