cc648436f8af1a07b927a09b6177ce0ffdeb9ca4
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-07-30 17:31:44 -0300

    add location input

--- 03-asynchronous-nodejs/app.js
+++ 03-asynchronous-nodejs/app.js
@@ -1,12 +1,25 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Chicago', (error, data) => {
-    console.log('Error:', error);
-    console.log('Data:', data);
-});
+const location = process.argv[2];
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error:', error);
-    console.log('Data:', data);
-});
+if (!location) {
+    console.log('Please provide a location.');
+} else {
+    console.log('Getting forecast for ' + location + '...');
+
+    geocode(location, (error, data) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            }
+
+            console.log(data.location);
+            console.log(`${forecastData.summary} It is currently ${forecastData.temperature}°C out. There is a ${forecastData.precipProbability}% chance of rain.`);
+        });
+    });
+}
