f01a6cbad55836ad7463d21da7088abbbbee3949
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-08-14 11:45:53 -0300

    add 404 page and wildcard for routes

--- 04-web-servers-with-express/src/app.js
+++ 04-web-servers-with-express/src/app.js
@@ -51,6 +51,22 @@ app.get('/weather', (req, res) => {
     });
 });
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Help article not found.',
+        name: 'brunv'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Page not found.',
+        name: 'brunv'
+    });
+});
+
 app.listen(3000, () => {
     console.log('Up and running on port 3000!');
 });
