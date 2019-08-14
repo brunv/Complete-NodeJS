9882960b3988686aeb81e991d593b4de02ec7fec
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-08-13 23:25:45 -0300

    serving html in public folder

--- 04-web-servers-with-express/src/app.js
+++ 04-web-servers-with-express/src/app.js
@@ -1,27 +1,31 @@
+const path = require('path');
 const express = require('express');
 
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(path.join(__dirname, '../public'));
+
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-    // sending back HTML
-    res.send('<h1>Hello express!</h1>');
-});
+app.use(express.static(publicDirectoryPath));
 
-app.get('/help', (req, res) => {
-    // sending back JSON
-    res.send([{
-        name: 'Andrew',
-        age: 27
-    }, {
-        name: 'Sarah',
-        age: 34
-    }
-    ]);
-});
+// app.get('/help', (req, res) => {
+//     // sending back JSON
+//     res.send([{
+//         name: 'Andrew',
+//         age: 27
+//     }, {
+//         name: 'Sarah',
+//         age: 34
+//     }
+//     ]);
+// });
 
-app.get('/about', (req, res) => {
-    res.send('<h2>This is the about page!</h2>');
-});
+// app.get('/about', (req, res) => {
+//     // sending back HTML
+//     res.send('<h2>This is the about page!</h2>');
+// });
 
 app.get('/weather', (req, res) => {
     res.send({
