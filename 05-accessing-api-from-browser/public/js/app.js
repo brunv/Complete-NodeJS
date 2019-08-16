6acb4850cd9734ab65c991369b798bb0228348a8
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-08-15 21:45:10 -0300

    wire up client and server to get forecast

--- 05-accessing-api-from-browser/public/js/app.js
+++ 05-accessing-api-from-browser/public/js/app.js
@@ -2,14 +2,27 @@
 
 console.log('Client side JS file is loaded!');
 
-// This is asynchronous
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((parsedData) => {
-        if (parsedData.error) {
-            console.log(parsedData.error);
-        } else {
-            console.log(parsedData.location);
-            console.log(parsedData.forecast);
-        }
+function getWeather(location) {
+    // This is asynchronous
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((parsedData) => {
+            if (parsedData.error) {
+                console.log(parsedData.error);
+            } else {
+                console.log(parsedData.location);
+                console.log(parsedData.forecast);
+            }
+        });
     });
+}
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    getWeather(location);
 });
