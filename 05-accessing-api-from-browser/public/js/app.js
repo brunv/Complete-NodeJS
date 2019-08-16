c5a7614f7c7efdd77069e51051dea7f0b7816903
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-08-15 21:31:33 -0300

    fetch weather api

--- 05-accessing-api-from-browser/public/js/app.js
+++ 05-accessing-api-from-browser/public/js/app.js
@@ -1,1 +1,15 @@
+/* This is client-side JavaScript so it can access browser APIs! */
+
 console.log('Client side JS file is loaded!');
+
+// This is asynchronous
+fetch('http://localhost:3000/weather?address=boston').then((response) => {
+    response.json().then((parsedData) => {
+        if (parsedData.error) {
+            console.log(parsedData.error);
+        } else {
+            console.log(parsedData.location);
+            console.log(parsedData.forecast);
+        }
+    });
+});
