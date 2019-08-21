2f349775d8413c1933bbda43c0384b57cef0ad4f
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-08-21 11:57:22 -0300

    add find example with cursor

--- 06-mongodb/mongodb.js
+++ 06-mongodb/mongodb.js
@@ -7,17 +7,19 @@ const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
 const id = new ObjectID();
-console.log(id);
+// console.log(id);
 // console.log(id.toHexString().length);
 // console.log(id.getTimestamp());
 
 // Connecting to the database:
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database!');
     }
 
     const db = client.db(databaseName);
+
+    /* CREATE */
 
     // Insert a single document to the 'users' collection:
     // (this is asynchronous)
@@ -55,4 +57,40 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     //     console.log(result.ops);
     // });
+
+    /* READ */
+
+    db.collection('tasks').findOne({
+        _id: new ObjectID("5d5cac826dafaa3d7c3ef110")
+    }, (error, task) => {
+        if (error) {
+            return console.log('Unable to fetch.');
+        }
+
+        if (task === null) {
+            console.log('No results.');
+        } else {
+            console.log(task);
+        }
+
+    });
+
+    /**
+     * 'find()' doesn't take in a callback as that second argument like other
+     * methods.What we get back as the return value is actually a 'cursos' and
+     * the 'cursor' is not the data we're asking for. It is a pointer to that 
+     * data in the database.
+     * And the reason we're getting a cursos back is that Mongo isn't going to
+     * assume that every time you use find you always want to get back an array
+     * of all of those documents. There are a lot of other things you might
+     * want so when we get a cursor back it opens a lot more possibilities.
+     * To work with it we just have to chain another mathod. Check documentation.
+     */
+    db.collection('tasks').find({ completed: false }).toArray((error, task) => {
+        console.log(task);
+    });
+
+    db.collection('tasks').find({ completed: false }).count((error, count) => {
+        console.log(count);
+    });
 });
