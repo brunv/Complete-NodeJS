9e1677eeb61f52d99cb13d80be2d530d499558a1
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-08-27 22:19:44 -0300

    model and insertion with mongoose

--- 07-rest-api-and-mongoose/src/db/mongoose.js
+++ 07-rest-api-and-mongoose/src/db/mongoose.js
@@ -4,3 +4,29 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
 });
+
+/* MODEL */
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+});
+
+/* MODEL INSTANCE */
+
+const me = new User({
+    name: 'Bruno',
+    age: 23
+});
+
+/* SAVE DATA */
+
+me.save().then(() => {
+    console.log(me);
+}).catch(() => {
+    console.log('Error: ', error);
+});
