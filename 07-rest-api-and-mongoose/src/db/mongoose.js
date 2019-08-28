30395056a71a8b5e933d7af2d943e9d301a2a008
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-08-28 09:46:17 -0300

    mongoose practice

--- 07-rest-api-and-mongoose/src/db/mongoose.js
+++ 07-rest-api-and-mongoose/src/db/mongoose.js
@@ -11,11 +11,25 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
     name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password".');
+            }
+        }
     },
     email: {
         type: String,
         required: true,
+        trim: true,
+        lowercase: true,
         validate(value) {
             if (!validator.isEmail(value)) {
                 throw new Error('Email is invalid');
@@ -24,6 +38,7 @@ const User = mongoose.model('User', {
     },
     age: {
         type: Number,
+        default: 0,
         validate(value) {
             if (value < 0) {
                 throw new Error('Age must be a positive number.');
@@ -34,36 +49,40 @@ const User = mongoose.model('User', {
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 });
 
 /* MODEL INSTANCE */
 
-const me = new User({
-    name: 'Bruno',
-    email: 'bruno_vieira95@hotmail.com',
-    age: 23
-});
-
-// const task = new Task({
-//     description: 'This is the very first task.',
-//     completed: true
+// const me = new User({
+//     name: 'Bruno',
+//     password: 'm1pH0n3',
+//     email: 'bruno_vieira95@hotmail.com',
+//     age: 23
 // });
+
+const task = new Task({
+    description: 'This is a new task.',
+    completed: true
+});
 
 /* SAVE DATA */
 
-me.save().then(() => {
-    console.log(me);
-}).catch((error) => {
-    console.log('Error: ', error);
-});
-
-// task.save().then(() => {
-//     console.log(task);
+// me.save().then(() => {
+//     console.log(me);
 // }).catch((error) => {
 //     console.log('Error: ', error);
 // });
+
+task.save().then(() => {
+    console.log(task);
+}).catch((error) => {
+    console.log('Error: ', error);
+});
