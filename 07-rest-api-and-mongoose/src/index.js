91ac8c220c82895a4e53fcdf955b3c9442c611a7
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-08-28 10:50:24 -0300

    add create user in api

--- 07-rest-api-and-mongoose/src/index.js
+++ 07-rest-api-and-mongoose/src/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+require('./db/mongoose');
+const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;  // Heroku or local
 
+app.use(express.json());
+
 app.post('/users', (req, res) => {
-    res.send('testing!');
+    // Create user with the request data:
+    const user = new User(req.body);
+
+    // Save the new user to DB:
+    user.save().then(() => {
+        res.send(user);
+    }).catch((error) => {
+        res.status(400).send(error);
+    });
 });
 
 app.listen(port, () => {
