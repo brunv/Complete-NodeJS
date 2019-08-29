3ffaade3c82ceebb36be09d956b557cf1a1aa269
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-08-29 14:23:03 -0300

    refactor promise chain to async await

--- 07-rest-api-and-mongoose/playground/promise-chaining-2.js
+++ 07-rest-api-and-mongoose/playground/promise-chaining-2.js
@@ -2,11 +2,28 @@ require('../src/db/mongoose');
 
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5d6676fcc7c8af48c067dc0e').then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-}).then((result) => {
-    console.log(result);
+/* Using Promise Chainning: */
+
+// Task.findByIdAndDelete('5d6676fcc7c8af48c067dc0e').then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+// }).then((result) => {
+//     console.log(result);
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+/* Using Async/Await: */
+
+const deleteTaskAndCount = async (id) => {
+    await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: false });
+
+    return count;
+};
+
+deleteTaskAndCount('5d65d82442694f439c283cbd').then((count) => {
+    console.log(count);
 }).catch((e) => {
     console.log(e);
-});
+})
--- 07-rest-api-and-mongoose/playground/promise-chaining.js
+++ 07-rest-api-and-mongoose/playground/promise-chaining.js
@@ -5,11 +5,28 @@ const User = require('../src/models/user');
 // Using promise chain to do one thing after another.
 // In this case we're updating an specific user and looking for
 // users that has the age = 1.
-User.findByIdAndUpdate('5d6684e90209ce525873e526', { age: 1 }).then((user) => {
-    console.log(user);
-    return User.countDocuments({ age: 1 });
-}).then((result) => {
-    console.log(result)
+
+// User.findByIdAndUpdate('5d6684e90209ce525873e526', { age: 1 }).then((user) => {
+//     console.log(user);
+//     return User.countDocuments({ age: 1 });
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+// This is the same thing up above but instead of Promise Chaining
+// we're using Async/Await:
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({ age });
+
+    return count;
+};
+
+updateAgeAndCount('5d6684e90209ce525873e526', 2).then((count) => {
+    console.log(count);
 }).catch((e) => {
     console.log(e);
-});
+})
