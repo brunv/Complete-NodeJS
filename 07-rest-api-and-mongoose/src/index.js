2b133a8f258b90defd7330bc7eb644458a99429e
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-08-29 15:16:35 -0300

    refactor routes to async wait from promise chain

--- 07-rest-api-and-mongoose/src/index.js
+++ 07-rest-api-and-mongoose/src/index.js
@@ -8,71 +8,105 @@ const port = process.env.PORT || 3000;  // Heroku or local
 
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-    // Fetch all users:
-    User.find({}).then((users) => {
+/** Routes for Users **/
+
+app.get('/users', async (req, res) => {
+    // Fetch all users (async/await):
+    try {
+        const users = await User.find({});
         res.send(users);
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send();
-    });
+    }
+    // Promise Chaining:
+    // User.find({}).then((users) => {
+    //     res.send(users);
+    // }).catch((error) => {
+    //     res.status(500).send();
+    // });
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     // console.log(req.params);
     const _id = req.params.id;
 
-    // Fetch an specific user by id:
-    User.findById(_id).then((user) => {
+    // Fetch an specific user by id (async/await):
+    try {
+        const user = await User.findById(_id);
+
         if (!user) {
             return res.status(404).send('User not found!');
         }
+
         res.send(user);
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send();
-    });
+    }
+    // Promise Chaining:
+    // User.findById(_id).then((user) => {
+    //     if (!user) {
+    //         return res.status(404).send('User not found!');
+    //     }
+    //     res.send(user);
+    // }).catch((error) => {
+    //     res.status(500).send();
+    // });
 });
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     // Create user with the request data:
     const user = new User(req.body);
 
-    // Save the new user to DB:
-    user.save().then(() => {
+    // Save the new user to DB (await):
+    try {
+        await user.save();
         res.status(201).send(user);
-    }).catch((error) => {
-        res.status(400).send(error);
-    });
+    } catch (e) {
+        res.status(400).send(e);
+    }
+    // Promise Chaining:
+    // Save the new user to DB (promise chain):
+    // user.save().then(() => {
+    //     res.status(201).send(user);
+    // }).catch((error) => {
+    //     res.status(400).send(error);
+    // });
 });
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+/** Routes for Tasks **/
+
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({});
         res.send(tasks);
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send();
-    });
+    }
 });
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id;
 
-    Task.findById(_id).then((task) => {
+    try {
+        const task = await Task.findById(_id);
         if (!task) {
-            return res.status(404).send('Task not found!');
+            res.status(404).send('Task not found!');
         }
         res.send(task);
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send();
-    });
+    }
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
 
-    task.save().then(() => {
+    try {
+        await task.save();
         res.status(201).send(task);
-    }).catch((error) => {
+    } catch (error) {
         res.status(400).send(error);
-    });
+    }
 });
 
 app.listen(port, () => {
