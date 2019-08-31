595be5c317b3be027eb34a1e3ba9f0af5541c8e8
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-08-31 15:04:04 -0300

    create hash password middleware

--- 08-authentication-and-security/src/models/user.js
+++ 08-authentication-and-security/src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -38,6 +39,28 @@ const User = mongoose.model('User', {
             }
         }
     }
+})
+
+/**
+ * Mongo already creates a schema under the hood when we pass the object
+ * directly into model. But doing this separately is going to allow us
+ * to take advantage of Middlewares.
+ */
+
+// Runs a specific function before 'save':
+userSchema.pre('save', async function (next) {
+    // Arrow functions doesn't work here because it does not bind 'this'.
+    const user = this;
+
+    if (user.isModified('password')) {
+        // the new hash override the plain text value:
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    // We simply call 'next()' when we're done:
+    next();
 });
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
