1753511f9832683ac2c33945ed0c6120f98cf46c
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-09-01 10:22:51 -0300

    add cascade task delete when user is deleted

--- 08-authentication-and-security/src/models/user.js
+++ 08-authentication-and-security/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 /**
  * Mongo already creates a schema under the hood when we pass the object
@@ -128,6 +129,15 @@ userSchema.pre('save', async function (next) {
     }
 
     // We simply call 'next()' when we're done:
+    next();
+});
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this;
+
+    await Task.deleteMany({ owner: user._id });
+
     next();
 });
 
