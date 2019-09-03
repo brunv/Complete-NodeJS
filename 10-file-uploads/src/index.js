7ad9f6b391910b397d2bf05b5381257c9103fe69
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-09-03 14:51:05 -0300

    add upload validation examples

--- 10-file-uploads/src/index.js
+++ 10-file-uploads/src/index.js
@@ -9,7 +9,23 @@ const port = process.env.PORT || 3000;  // Heroku or local
 // Example of file upload:
 const multer = require('multer');
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000,
+    },
+    fileFilter(req, file, cb) {
+        // if (!file.originalname.endsWith('.pdf')) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document.'))
+        }
+
+        cb(undefined, true);
+
+        // Calbacks:
+        // cb(new Error('File must be a PDF.'));   // send error back
+        // cb(undefined, true);    // no error and accepts upload
+        // cb(undefined, false);   // no error and rejects upload
+    }
 });
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send();
