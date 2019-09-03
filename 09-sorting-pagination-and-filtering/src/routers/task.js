b26f832edf568387d864133abe7cf6cf95ba3e86
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-09-02 22:54:10 -0300

    add sorting for tasks

--- 09-sorting-pagination-and-filtering/src/routers/task.js
+++ 09-sorting-pagination-and-filtering/src/routers/task.js
@@ -6,11 +6,18 @@ const router = express.Router();
 
 // GET /tasks?completed=true
 // GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {};
+    const sort = {};
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true';
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
     }
 
     try {
@@ -19,7 +26,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate();
         res.send(req.user.tasks);
