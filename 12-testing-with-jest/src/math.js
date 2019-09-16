f564bcecf70a998d65f4902792c4e2e014a0e914
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-09-15 22:49:21 -0300

    add test example using expect method

--- 12-testing-with-jest/src/math.js
+++ 12-testing-with-jest/src/math.js
@@ -2,10 +2,7 @@
  * This is just an example for learning tests with Jest.
  */
 
-const calculateTip = (total, tipPercent) => {
-    const tip = total * tipPercent;
-    return total + tip;
-};
+const calculateTip = (total, tipPercent = .25) => total + (total * tipPercent);
 
 module.exports = {
     calculateTip
