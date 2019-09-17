7fbc4ccbb9e5fb1c755b2e4f277f08ab531119d0
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-09-17 13:43:36 -0300

    making assertions about the response

--- 12-testing-with-jest/tests/user.test.js
+++ 12-testing-with-jest/tests/user.test.js
@@ -32,18 +32,37 @@ beforeEach(async () => {
 // });
 
 test('Should signup a new user', async () => {
-    await request(app).post('/users').send({
+    const response = await request(app).post('/users').send({
         name: 'Andrew',
         email: 'andrew@example.com',
         password: 'MyPass777!'
     }).expect(201);
+
+    // Assert that the user was inserted in the database correctly:
+    const user = await User.findById(response.body.user._id);
+    expect(user).not.toBeNull();
+
+    // Assertions about the response:
+    // expect(response.body.user.name).toBe('Andrew'); // this checks up only one property
+    expect(response.body).toMatchObject({
+        // the properties we list out here do need to match up exactly
+        user: {
+            name: 'Andrew',
+            email: 'andrew@example.com'
+        },
+        token: user.tokens[0].token
+    });
+    expect(user.password).not.toBe('MyPass777!');
 });
 
 test('Should login existing user', async () => {
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email: userOne.email,
         password: userOne.password
     }).expect(200);
+
+    const user = await User.findById(response.body.user._id);
+    expect(user.tokens[1].token).toBe(response.body.token);
 });
 
 test('Should not login nonexistent user', async () => {
@@ -74,6 +93,9 @@ test('Should delete account for user', async () => {
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user).toBeNull();
 });
 
 test('Should not delete account for unauthenticated user', async () => {
