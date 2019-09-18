4ae7b99dd6093b55fa01ba1faa6ef4462e02dba9
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-09-18 11:02:09 -0300

    real-time example with socketio

--- 13-real-time-with-sockets/public/js/chat.js
+++ 13-real-time-with-sockets/public/js/chat.js
@@ -1,1 +1,15 @@
-io();
+/**
+ * That's the same 'socket' we already have on the server
+ * when the connection comes in. This is going to allow us to
+ * send events and receive events from both server and the client.
+ */
+const socket = io();
+
+socket.on('countUpdated', (count) => {
+    console.log('The count has been updated!', count);
+});
+
+document.querySelector('#increment').addEventListener('click', () => {
+    console.log('Clicked!');
+    socket.emit('increment');
+});
--- 13-real-time-with-sockets/src/index.js
+++ 13-real-time-with-sockets/src/index.js
@@ -15,8 +15,29 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+let count = 0;
+
+/**
+ * What is happening?
+ * server (emit) -> client (receive) -> countUpdated
+ * client (emit) -> server (receive) -> increment
+ */
+
+io.on('connection', (socket) => {
     console.log('New WebSocket connection');
+
+    // send event from server:
+    socket.emit('countUpdated', count);
+
+    socket.on('increment', () => {
+        count++;
+
+        // Here we are emitting the event to a particular connection:
+        // socket.emit('countUpdated', count);
+
+        // We want to emit it to every connection available:
+        io.emit('countUpdated', count);
+    });
 });
 
 server.listen(port, () => console.log(`Server up and running on port ${port}.`));
